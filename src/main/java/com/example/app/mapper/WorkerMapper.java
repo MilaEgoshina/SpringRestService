9a1066e06a4d084517ec50ac65a5e6d31d96941e
mila <milavoronina1997@mail.ru>
2024-07-06 21:44:05 +0500

    refactor: correct worker mapper interface for update

--- src/main/java/com/example/app/mapper/WorkerMapper.java
+++ src/main/java/com/example/app/mapper/WorkerMapper.java
@@ -1,44 +1,74 @@
 package com.example.app.mapper;
 
-import com.example.app.dto.IncomingWorkerDTO;
-import com.example.app.dto.OutgoingFullWorkerDTO;
-import com.example.app.dto.UpdateWorkerDTO;
+import com.example.app.dto.*;
+import com.example.app.entity.Computer;
+import com.example.app.entity.Role;
+import com.example.app.entity.WorkRelations;
 import com.example.app.entity.Worker;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(uses = {RoleMapper.class, ComputerMapper.class, WorkRelationsMapper.class})
 public interface WorkerMapper {
+
+       RoleMapper roleMapper = RoleMapper.INSTANCE;
+       ComputerMapper computerMapper = ComputerMapper.INSTANCE;
+       WorkRelationsMapper workRelationsMapper = WorkRelationsMapper.INSTANCE;
+
 
     WorkerMapper INSTANCE = Mappers.getMapper(WorkerMapper.class);
 
     @Mappings({
-            @Mapping(target = "role", source = "role"),
-            @Mapping(target = "workRelationsList", source = "workRelationsList", qualifiedByName = "outgoingWorkRelationsDTOS"),
-            @Mapping(target = "computerList", source = "computerList", qualifiedByName = "outgoingComputerNumberDTOS"),
+            @Mapping(target = "id", ignore = true),
+            @Mapping(target = "computerList", ignore = true),
+            @Mapping(target = "workRelationsList", ignore = true),
     })
     Worker mapToEntity(IncomingWorkerDTO incomingWorkerDTO);
 
     @Mappings({
-            @Mapping(target = "role", source = "role.id"),
-            @Mapping(target = "workRelationsList", source = "workRelationsList", qualifiedByName = "outgoingWorkRelationsDTOS"),
-            @Mapping(target = "computerList", source = "computerList", qualifiedByName = "outgoingComputerNumberDTOS"),
+            @Mapping(target = "role", source = "role", qualifiedByName = "roleMapping"),
+            @Mapping(target = "workRelationsList", source = "workRelationsList", qualifiedByName = "workRelationsListUpdateMapping"),
+            @Mapping(target = "computerList", source = "computerList", qualifiedByName = "computerListMapping"),
 
     })
     OutgoingFullWorkerDTO mapToDTO(Worker worker);
 
     @Mappings({
-            @Mapping(target = "id", source = "id"),
-            @Mapping(target = "role", source = "role"),
-            @Mapping(target = "workRelationsList", source = "workRelationsList", qualifiedByName = "outgoingWorkRelationsDTOS"),
-            @Mapping(target = "computerList", source = "computerList", qualifiedByName = "outgoingComputerNumberDTOS"),
+            @Mapping(target = "role", source = "role",qualifiedByName = "roleMapping"),
+            @Mapping(target = "workRelationsList", source = "workRelationsList", qualifiedByName = "workRelationsListUpdateMapping"),
+            @Mapping(target = "computerList", source = "computerList", qualifiedByName = "computerListUpdateMapping"),
     })
     Worker update(UpdateWorkerDTO updateWorkerDTO);
 
     List<OutgoingFullWorkerDTO> mapToOutGoingDtos(List<Worker> workerList);
 
+    @Named("roleMapping")
+    default OutgoingRoleDTO roleToDto(Role role) {
+        return roleMapper.mapToDTO(role);
+    }
+    @Named("computerListMapping")
+    default List<OutgoingComputerDTO> computerMapper(List<Computer> computerDTOList) {
+        return computerMapper.mapToOutGoingDtos(computerDTOList);
+    }
+
+
+    @Named("workRelationsListMapping")
+    default List<OutgoingWorkRelationsDTO> workRelationsMapper(List<WorkRelations> workRelationsDTOList) {
+        return workRelationsMapper.mapToOutGoingDtos(workRelationsDTOList);
+    }
+    @Named("computerListUpdateMapping")
+    default List<Computer> computerUpdateMapper(List<UpdateComputerDTO> computerDTOList) {
+        return computerMapper.mapToUpdateEntities(computerDTOList);
+    }
+
+    @Named("workRelationsListUpdateMapping")
+    default List<WorkRelations>  workRelationsUpdateMapper(List<UpdateWorkRelationsDTO> workRelationsDTOList) {
+        return workRelationsMapper.mapToUpdateEntities(workRelationsDTOList);
+    }
 }
+
