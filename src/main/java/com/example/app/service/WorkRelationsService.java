2edecf29a9dcdd34918619a429a72f0a14e01afe
mila <milavoronina1997@mail.ru>
2024-07-07 18:13:42 +0500

    dev: create service layer for work relations repository

--- src/main/java/com/example/app/service/WorkRelationsService.java
+++ src/main/java/com/example/app/service/WorkRelationsService.java
@@ -4,11 +4,11 @@ import com.example.app.dto.IncomingWorkRelationsDTO;
 import com.example.app.dto.OutgoingWorkRelationsDTO;
 import com.example.app.dto.UpdateWorkRelationsDTO;
 import com.example.app.entity.WorkRelations;
+import com.example.app.entity.Worker;
 import com.example.app.exceptions.NotFoundException;
 import com.example.app.mapper.WorkRelationsMapper;
 import com.example.app.repository.WorkRelationsRepository;
 import com.example.app.repository.WorkerRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,14 +18,20 @@ import java.util.Optional;
 @Service
 public class WorkRelationsService {
 
-    @Autowired
-    private WorkRelationsRepository workerRelationsRepository;
 
-    @Autowired
-    private WorkerRepository workerRepository;
+    private final WorkRelationsRepository workerRelationsRepository;
 
-    @Autowired
-    private WorkRelationsMapper workRelationsMapper;
+
+    private final WorkerRepository workerRepository;
+
+
+    private final WorkRelationsMapper workRelationsMapper;
+
+    public WorkRelationsService(WorkRelationsRepository workerRelationsRepository, WorkerRepository workerRepository, WorkRelationsMapper workRelationsMapper) {
+        this.workerRelationsRepository = workerRelationsRepository;
+        this.workerRepository = workerRepository;
+        this.workRelationsMapper = workRelationsMapper;
+    }
 
     @Transactional
     public OutgoingWorkRelationsDTO saveWorkRelations(IncomingWorkRelationsDTO incomingWorkRelationsDTO){
@@ -73,5 +79,46 @@ public class WorkRelationsService {
         }
     }
 
-    
+    public void addWorkerToRelations(Long id, Long workerId) throws NotFoundException{
+        checkExistWorkRelations(id);
+        Optional<WorkRelations> workRelationsOptional = workerRelationsRepository.findById(id);
+        Optional<Worker> workerOptional = workerRepository.findById(workerId);
+        if(workerOptional.isPresent() && workRelationsOptional.isPresent()){
+            WorkRelations workRelationsExisting= workRelationsOptional.get();
+            Worker workerExisting = workerOptional.get();
+            workRelationsExisting.getWorkerList().add(workerExisting);
+            workerExisting.getWorkRelationsList().add(workRelationsExisting);
+
+            workerRelationsRepository.save(workRelationsExisting);
+            workerRepository.save(workerExisting);
+        }else{
+            throw new NotFoundException("Entities not found");
+        }
+    }
+
+    public void deleteWorkerFromRelations(Long id, Long workerId) throws NotFoundException{
+
+        checkExistWorkRelations(id);
+        Optional<WorkRelations> workRelationsOptional = workerRelationsRepository.findById(id);
+        Optional<Worker> workerOptional = workerRepository.findById(workerId);
+        if(workerOptional.isPresent() && workRelationsOptional.isPresent()) {
+            WorkRelations workRelationsExisting = workRelationsOptional.get();
+            Worker workerExisting = workerOptional.get();
+
+            workRelationsExisting.getWorkerList().remove(workerExisting);
+            workerExisting.getWorkRelationsList().remove(workRelationsExisting);
+
+            workerRelationsRepository.save(workRelationsExisting);
+            workerRepository.save(workerExisting);
+
+        }else{
+            throw new NotFoundException("Entities not found");
+        }
+
+    }
+    private void checkExistWorkRelations(Long id) throws NotFoundException {
+        if (!workerRelationsRepository.existsById(id)) {
+            throw new NotFoundException("WorkRelations not found.");
+        }
+    }
 }
